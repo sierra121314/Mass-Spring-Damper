4ae02021d3038f632d59b0f6dc265561d3e44fbd
sierra121314 <sierrag@nevada.unr.edu>
2017-09-18 09:11:03 -0700

    updated - pro and ant force-fitness

--- plot_displacement_2.m
+++ plot_displacement_2.m
@@ -1,26 +1,55 @@
-close all
+clear all, clc, close all
 % When importing new data
-% 1) Home tab
-% 2) Clear Workspace - deletes variables
-% 3) Import Data
 
-data = load('x_history.txt')
+data1 = load('x_history.txt');
+data2 = load('x_dot_history.txt');
+data3 = load('x_dd_history.txt');
+data4 = load('P_force_history.txt');
+data5 = load('A_force_history.txt');
+data6 = load('P_best_fitness_history.txt');
+data7 = load('A_best_fitness_history.txt');
 
-subplot(3,1,1)
-plot(vectorspot, x)
+time = [1:500];
+generations = [1:100];
+
+subplot(7,1,1)
+plot(time, data1)
 title('displacement')
 ylabel('displacement')
-xlabel('time[s]') %%policies?
+xlabel('time[ms]') 
 
-subplot(3,1,2)
-plot(vectorspot, x_dot)
+subplot(7,1,2)
+plot(time, data2)
 title('velocity')
 ylabel('velocity')
-xlabel('time[s]')
+xlabel('time[ms]')
 
-subplot(3,1,3)
-plot(vectorspot, x_dd)
+subplot(7,1,3)
+plot(time, data3)
 title('acceleration')
 ylabel('acceleration')
-xlabel('time[s]')
+xlabel('time[ms]')
 
+subplot(7,1,4)
+plot(time, data4)
+title('Protagonist force')
+ylabel('P force')
+xlabel('time[ms]')
+
+subplot(7,1,5)
+plot(time, data5)
+title('Antagonist force')
+ylabel('A force')
+xlabel('time[ms]')
+
+subplot(7,1,6)
+plot(generations, data6)
+title('Best Protagonist fitness')
+ylabel('best fitness so far')
+xlabel('generations')
+
+subplot(7,1,7)
+plot(generations, data7)
+title('Best Antagonist fitness')
+ylabel('best fitness so far')
+xlabel('generations')
